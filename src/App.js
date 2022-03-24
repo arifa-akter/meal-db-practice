4e202b179e402ec9f6feaddcfa1cd6ba2f02db4a
arifa-akter <arifaakterchoity@gmail.com>
2022-03-25 00:56:40 +0600

    react file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import './App.css';
-
+import Navber from './components/Navber/Navber';
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
-      
+      <Navber></Navber>
     </div>
   );
 }
---
+++ src/components/Navber/Navber.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Navber.css'
+const Navber = () => {
+    return (
+        <div>
+            <h1 className='text-danger'>product</h1>
+        </div>
+    );
+};
+
+export default Navber;
